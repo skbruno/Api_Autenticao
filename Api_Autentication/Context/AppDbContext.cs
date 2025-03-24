1f39b6b2d1fc39f55aa0e1043d329a5f5194f806
skbruno <bruno.rodriguesc9@gmail.com>
2025-03-24 20:01:27 -0300

    Inicialização repo

---
+++ Api_Autentication/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Api_Autentication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api_Autentication.AppDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Api_Autentication/Models/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Api_Autentication.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public byte[] PasswordHash { get; private set; }
+        public byte[] PasswordSalt { get; private set; }
+
+        public Usuario(int usuarioId, string nome, string email)
+        {
+            UsuarioId = usuarioId;
+            Nome = nome;
+            Email = email;
+        }
+
+        public Usuario(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public void AlterarSenha(byte[] passwordHash, byte[] passwordSalt)
+        {
+            PasswordHash = passwordHash;
+            PasswordHash = passwordSalt;
+        }
+    }
+}
